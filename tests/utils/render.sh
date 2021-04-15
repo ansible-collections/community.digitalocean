5cbfaa0da1459f9ba3ea207f63d4cffe0f79f118
John Barker <john@johnrbarker.com>
2021-04-15 18:25:42 +0100

    Integration Tests

---
+++ tests/utils/render.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Renders tests/integration/integration_config.yml
+
+set -e
+set -o pipefail
+set -u
+
+function main()
+{
+  readonly template="$1"; shift
+  readonly content="$(cat "$template")"
+
+  eval "echo \"$content\""
+}
+
+main "$@"
