a964e5baacd98c6a6d0bded71e767e45f9d7db80
Mark Mercado <mamercad@gmail.com>
2021-04-21 13:42:40 -0400

    Support getting a single database

--- plugins/modules/digital_ocean_database_info.py
+++ plugins/modules/digital_ocean_database_info.py
@@ -178,9 +178,25 @@ class DODatabaseInfo(object):
 
 def run(module):
     state = module.params.pop('state')
+    id = module.params.get('id', None)
+    name = module.params.get('name', None)
+
     database = DODatabaseInfo(module)
-    all_databases = database.get_databases()
-    module.exit_json(changed=False, data=all_databases)
+
+    if id is not None or name is not None:
+      the_database = database.get_database()
+      if the_database:  # Found it
+        module.exit_json(changed=False, data=the_database)
+      else:  # Didn't find it
+        if id is not None and name is not None:
+          module.fail_json(change=False, msg='Database {0} ({1}) not found'.format(id, name))
+        elif id is not None and name is None:
+          module.fail_json(change=False, msg='Database {0} not found'.format(id))
+        elif id is None and name is not None:
+          module.fail_json(change=False, msg='Database {0} not found'.format(name))
+    else:
+      all_databases = database.get_databases()
+      module.exit_json(changed=False, data=all_databases)
 
 
 def main():
