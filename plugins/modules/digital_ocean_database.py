d586785b8163d6dd094a2444f9338e3dbb431a23
Mark Mercado <mamercad@gmail.com>
2021-04-21 13:50:11 -0400

    Update Copyright

--- plugins/modules/digital_ocean_database.py
+++ plugins/modules/digital_ocean_database.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 #
 # Copyright: Ansible Project
+# Copyright: (c) 2021, Mark Mercado <mmercado@digitalocean.com>
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
 from __future__ import absolute_import, division, print_function
