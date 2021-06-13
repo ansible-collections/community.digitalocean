1098d20e99d6c769a59cc3795499716ed77fb9ca
Mark Mercado <mamercad@gmail.com>
2021-06-13 14:20:48 -0400

    Adding version_added

--- plugins/modules/digital_ocean_snapshot.py
+++ plugins/modules/digital_ocean_snapshot.py
@@ -13,6 +13,7 @@ DOCUMENTATION = r"""
 ---
 module: digital_ocean_snapshot
 short_description: Create and delete DigitalOcean snapshots
+version_added: 1.6.1
 description:
   - This module can be used to create and delete DigitalOcean Droplet and volume snapshots.
 author: "Mark Mercado (@mamercad)"
