1ac2d7bd4e7fada2b40dd73513f1af811a4d98e5
Mark Mercado <mamercad@gmail.com>
2021-06-13 14:17:37 -0400

    Not using this

--- plugins/modules/digital_ocean_snapshot.py
+++ plugins/modules/digital_ocean_snapshot.py
@@ -121,7 +121,6 @@ result:
 
 
 import time
-from traceback import format_exc
 from ansible.module_utils.basic import AnsibleModule
 from ansible_collections.community.digitalocean.plugins.module_utils.digital_ocean import (
     DigitalOceanHelper,
