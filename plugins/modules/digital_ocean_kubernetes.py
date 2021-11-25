be1054f76cf201afde7a1cab0233560ca2a26369
Mark Mercado <mamercad@gmail.com>
2021-11-25 12:49:46 -0500

    Stubbing out unit tests

--- plugins/modules/digital_ocean_kubernetes.py
+++ plugins/modules/digital_ocean_kubernetes.py
@@ -311,11 +311,10 @@ class DOKubernetes(object):
         response = self.rest.get(
             "kubernetes/clusters/{0}/kubeconfig".format(self.cluster_id)
         )
-        return response.body
-        # if response.status_code == 200:
-        #     return response.body
-        # else:
-        #     self.module.fail_json(msg="Failed to retrieve kubeconfig")
+        if response.status_code == 200:
+            return response.body
+        else:
+            self.module.fail_json(msg="Failed to retrieve kubeconfig")
 
     def get_kubernetes(self):
         """Returns an existing DigitalOcean Kubernetes cluster by name"""
---
+++ tests/unit/plugins/modules/test_digital_ocean_kubernetes.py
@@ -0,0 +1,13 @@
+from __future__ import absolute_import, division, print_function
+
+__metaclass__ = type
+
+import pytest
+
+from ansible.errors import AnsibleError, AnsibleParserError
+from ansible_collections.community.general.tests.unit.compat import mock
+
+import ansible_collections.community.digitalocean.plugins.modules.digital_ocean_kubernetes as module_under_test
+
+def test_get_kubernetes_kubeconfig():
+    assert True
