3a390652bd2d3f7c582f56d8cc5f64ecec75f3eb
Mark Mercado <mamercad@gmail.com>
2021-11-25 12:57:09 -0500

    Remove these for now

--- tests/unit/plugins/modules/test_digital_ocean_kubernetes_info.py
+++ tests/unit/plugins/modules/test_digital_ocean_kubernetes_info.py
@@ -4,8 +4,8 @@ __metaclass__ = type
 
 import pytest
 
-from ansible.errors import AnsibleError, AnsibleParserError
-from ansible_collections.community.general.tests.unit.compat import mock
+# from ansible.errors import AnsibleError, AnsibleParserError
+# from ansible_collections.community.general.tests.unit.compat import mock
 
 import ansible_collections.community.digitalocean.plugins.modules.digital_ocean_kubernetes_info as module_under_test
 
