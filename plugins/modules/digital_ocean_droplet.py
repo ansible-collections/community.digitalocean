5c52f13c5ce7847617b9695f8d933c25137b3c5d
Mark Mercado <mmercado@digitalocean.com>
2021-12-14 12:56:37 -0500

    This block is flawed and unnecessary

--- plugins/modules/digital_ocean_droplet.py
+++ plugins/modules/digital_ocean_droplet.py
@@ -767,11 +767,6 @@ class DODroplet(object):
             if state == "inactive":
                 self.ensure_power_off(droplet_id)
 
-        # Get updated Droplet data (fallback to current data)
-        if self.wait:
-            json_data = self.get_droplet()
-            droplet = json_data.get("droplet", droplet)
-
         project_name = self.module.params.get("project")
         if project_name:  # empty string is the default project, skip project assignment
             urn = "do:droplet:{0}".format(droplet_id)
