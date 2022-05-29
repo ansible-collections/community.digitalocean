616bf0e0b7b2099ebd46de104208336d2f9af2a4
Mark Mercado <mamercad@gmail.com>
2022-05-29 12:00:22 -0400

    Iterating

--- plugins/modules/digital_ocean_balance_info.py
+++ plugins/modules/digital_ocean_balance_info.py
@@ -3,7 +3,7 @@
 # Copyright: (c) 2021, Ansible Project
 # Copyright: (c) 2021, Mark Mercado <mamercad@gmail.com>
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
-# TESTING
+# TESTING1
 
 from __future__ import absolute_import, division, print_function
 
