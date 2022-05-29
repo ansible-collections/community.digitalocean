b67b8ba045cc217cac992a0d49617a1927819143
Mark Mercado <mamercad@gmail.com>
2022-05-29 12:02:58 -0400

    Iterating

--- plugins/modules/digital_ocean_account_info.py
+++ plugins/modules/digital_ocean_account_info.py
@@ -3,7 +3,6 @@
 # Copyright: (c) 2018, Ansible Project
 # Copyright: (c) 2018, Abhijeet Kasurde <akasurde@redhat.com>
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
-# TESTING1
 
 from __future__ import absolute_import, division, print_function
 
