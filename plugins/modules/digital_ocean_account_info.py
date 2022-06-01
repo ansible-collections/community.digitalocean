064b5c86a59d5fb38a8836ea1f278dcfa6558678
Mark Mercado <mamercad@gmail.com>
2022-06-01 18:30:04 -0400

    Linting (testing CI for PRs)

--- plugins/modules/digital_ocean_account_info.py
+++ plugins/modules/digital_ocean_account_info.py
@@ -14,16 +14,13 @@ DOCUMENTATION = r"""
 module: digital_ocean_account_info
 short_description: Gather information about DigitalOcean User account
 description:
-    - This module can be used to gather information about User account.
-    - This module was called C(digital_ocean_account_facts) before Ansible 2.9. The usage did not change.
+  - This module can be used to gather information about User account.
+  - This module was called C(digital_ocean_account_facts) before Ansible 2.9. The usage did not change.
 author: "Abhijeet Kasurde (@Akasurde)"
-
 requirements:
   - "python >= 2.6"
-
 extends_documentation_fragment:
 - community.digitalocean.digital_ocean.documentation
-
 """
 
 
