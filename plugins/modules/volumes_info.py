a57d4f6117330641ce72421fba3db4536e1f6934
Mark Mercado <mamercad@gmail.com>
2022-11-24 13:58:06 -0500

    Add volumes_info module and tests

---
+++ plugins/modules/volumes_info.py
@@ -0,0 +1,106 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# Copyright: (c) 2022, Mark Mercado <mmercado@digitalocean.com>
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+from __future__ import absolute_import, division, print_function
+
+__metaclass__ = type
+
+
+DOCUMENTATION = r"""
+---
+module: volumes_info
+
+short_description: Get current volumes
+
+version_added: 2.0.0
+
+description:
+  - This module gets the current volumes.
+
+author: Mark Mercado (@mamercad)
+
+requirements:
+  - pydo >= 0.1.3
+  - azure-core >= 1.26.1
+
+extends_documentation_fragment:
+  - community.digitalocean.token.documentation
+"""
+
+
+EXAMPLES = r"""
+- name: Get DigitalOcean volumes
+  community.digitalocean.volumes_info:
+    token: "{{ token }}"
+"""
+
+
+RETURN = r"""
+volumes:
+  description: DigitalOcean volumes information.
+  returned: success
+  type: dict
+  sample: {}
+"""
+
+from ansible.module_utils.basic import AnsibleModule, missing_required_lib
+from ansible_collections.community.digitalocean.plugins.module_utils.common_options import (
+    DigitalOceanOptions,
+)
+
+import traceback
+
+HAS_AZURE_LIBRARY = False
+AZURE_LIBRARY_IMPORT_ERROR = None
+try:
+    from azure.core.exceptions import HttpResponseError
+except ImportError:
+    AZURE_LIBRARY_IMPORT_ERROR = traceback.format_exc()
+else:
+    HAS_AZURE_LIBRARY = True
+
+HAS_PYDO_LIBRARY = False
+PYDO_LIBRARY_IMPORT_ERROR = None
+try:
+    from pydo import Client
+except ImportError:
+    PYDO_LIBRARY_IMPORT_ERROR = traceback.format_exc()
+else:
+    HAS_PYDO_LIBRARY = True
+
+
+def core(module):
+    client = Client(token=module.params.get("token"))
+    try:
+        volumes = client.volumes.list()
+        module.exit_json(changed=False, volumes=volumes)
+    except HttpResponseError as err:
+        error = {
+            "Message": err.error.message,
+            "Status Code": err.status_code,
+            "Reason": err.reason,
+        }
+        module.fail_json(changed=False, msg=error.get("Message"), error=error)
+
+
+def main():
+    argument_spec = DigitalOceanOptions.argument_spec()
+    module = AnsibleModule(argument_spec=argument_spec, supports_check_mode=True)
+    if not HAS_AZURE_LIBRARY:
+        module.fail_json(
+            msg=missing_required_lib("azure.core.exceptions"),
+            exception=AZURE_LIBRARY_IMPORT_ERROR,
+        )
+    if not HAS_PYDO_LIBRARY:
+        module.fail_json(
+            msg=missing_required_lib("pydo"),
+            exception=PYDO_LIBRARY_IMPORT_ERROR,
+        )
+
+    core(module)
+
+
+if __name__ == "__main__":
+    main()
