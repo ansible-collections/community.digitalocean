2b7ef0a6e9d6606ea1a19e87d6172318b3e624b2
Mark Mercado <mamercad@gmail.com>
2022-11-26 11:55:17 -0500

    Add billing history info module and tests

---
+++ plugins/modules/billing_history_info.py
@@ -0,0 +1,133 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# Copyright: (c) 2022, Mark Mercado <mmercado@digitalocean.com>
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+from __future__ import absolute_import, division, print_function
+
+__metaclass__ = type
+
+
+DOCUMENTATION = r"""
+---
+module: billing_history_info
+
+short_description: Get current user billing history information
+
+version_added: 2.0.0
+
+description:
+  - This module gets the current user billing history information.
+
+author: Mark Mercado (@mamercad)
+
+requirements:
+  - pydo >= 0.1.3
+  - azure-core >= 1.26.1
+
+extends_documentation_fragment:
+  - community.digitalocean.common.documentation
+"""
+
+
+EXAMPLES = r"""
+- name: Get DigitalOcean billing history information
+  community.digitalocean.billing_history_info:
+    token: "{{ token }}"
+"""
+
+
+RETURN = r"""
+billing_history:
+  description: DigitalOcean billing history information.
+  returned: success
+  type: dict
+  sample:
+    billing_history:
+      description: List of billing history activities.
+      returned: success
+      type: list
+      entries: dict
+      sample:
+        description: Invoice for May 2018
+        amount: 12.34
+        invoice_id: 123
+        invoice_uuid: example-uuid
+        date: '2018-06-01T08:44:38Z'
+        type: Invoice
+    meta:
+      description: Billing history metadata.
+      returned: success
+      type: dict
+      sample:
+        total: Total number of billing history entries.
+        type: int
+        sample: 5
+    links:
+      description: Links to next and last results.
+      returned: success
+      type: dict
+      sample:
+        next: 'https://api.digitalocean.com/v2/customers/my/billing_history?page=2&per_page=2'
+        last: 'https://api.digitalocean.com/v2/customers/my/billing_history?page=3&per_page=2'
+"""
+
+from ansible.module_utils.basic import AnsibleModule, missing_required_lib
+from ansible_collections.community.digitalocean.plugins.module_utils.common import (
+    DigitalOceanOptions,
+)
+
+import traceback
+
+HAS_AZURE_LIBRARY = False
+AZURE_LIBRARY_IMPORT_ERROR = None
+try:
+    from azure.core.exceptions import HttpResponseError
+except ImportError:
+    AZURE_LIBRARY_IMPORT_ERROR = traceback.format_exc()
+else:
+    HAS_AZURE_LIBRARY = True
+
+HAS_PYDO_LIBRARY = False
+PYDO_LIBRARY_IMPORT_ERROR = None
+try:
+    from pydo import Client
+except ImportError:
+    PYDO_LIBRARY_IMPORT_ERROR = traceback.format_exc()
+else:
+    HAS_PYDO_LIBRARY = True
+
+
+def core(module):
+    client = Client(token=module.params.get("token"))
+    try:
+        billing_history = client.billing_history.list()
+        module.exit_json(changed=False, billing_history=billing_history)
+    except HttpResponseError as err:
+        error = {
+            "Message": err.error.message,
+            "Status Code": err.status_code,
+            "Reason": err.reason,
+        }
+        module.fail_json(changed=False, msg=error.get("Message"), error=error)
+
+
+def main():
+    argument_spec = DigitalOceanOptions.argument_spec()
+    module = AnsibleModule(argument_spec=argument_spec, supports_check_mode=True)
+    if not HAS_AZURE_LIBRARY:
+        module.fail_json(
+            msg=missing_required_lib("azure.core.exceptions"),
+            exception=AZURE_LIBRARY_IMPORT_ERROR,
+        )
+    if not HAS_PYDO_LIBRARY:
+        module.fail_json(
+            msg=missing_required_lib("pydo"),
+            exception=PYDO_LIBRARY_IMPORT_ERROR,
+        )
+
+    core(module)
+
+
+if __name__ == "__main__":
+    main()
