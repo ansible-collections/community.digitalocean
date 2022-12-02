de2f7acb78e186ae12577d4dfb09dae1027b7039
Mark Mercado <mamercad@gmail.com>
2022-12-02 11:12:35 -0500

    Remove stray commas

--- plugins/modules/certificates_info.py
+++ plugins/modules/certificates_info.py
@@ -46,10 +46,10 @@ certificates:
   sample:
     - id: 892071a0-bb95-49bc-8021-3afd67a210bf
       name: web-cert-01
-      not_after: '2017-02-22T00:23:00Z',
+      not_after: '2017-02-22T00:23:00Z'
       sha1_fingerprint: dfcc9f57d86bf58e321c2c6c31c7a971be244ac7
       created_at: '2017-02-08T16:02:37Z'
-      dns_names: [],
+      dns_names: []
       state: verified
       type: custom
 msg:
