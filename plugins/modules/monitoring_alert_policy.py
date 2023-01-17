d7e640efb72122a337c1ca4f4445eb41154dd964
Mark Mercado <mamercad@gmail.com>
2023-01-17 07:58:48 -0500

    Fix choices

--- plugins/modules/monitoring_alert_policy.py
+++ plugins/modules/monitoring_alert_policy.py
@@ -66,34 +66,34 @@ options:
     type: str
     required: true
     choices:
-      - "v1/insights/droplet/load_1",
-      - "v1/insights/droplet/load_5",
-      - "v1/insights/droplet/load_15",
-      - "v1/insights/droplet/memory_utilization_percent",
-      - "v1/insights/droplet/disk_utilization_percent",
-      - "v1/insights/droplet/cpu",
-      - "v1/insights/droplet/disk_read",
-      - "v1/insights/droplet/disk_write",
-      - "v1/insights/droplet/public_outbound_bandwidth",
-      - "v1/insights/droplet/public_inbound_bandwidth",
-      - "v1/insights/droplet/private_outbound_bandwidth",
-      - "v1/insights/droplet/private_inbound_bandwidth",
-      - "v1/insights/lbaas/avg_cpu_utilization_percent",
-      - "v1/insights/lbaas/connection_utilization_percent",
-      - "v1/insights/lbaas/droplet_health",
-      - "v1/insights/lbaas/tls_connections_per_second_utilization_percent",
-      - "v1/insights/lbaas/increase_in_http_error_rate_percentage_5xx",
-      - "v1/insights/lbaas/increase_in_http_error_rate_percentage_4xx",
-      - "v1/insights/lbaas/increase_in_http_error_rate_count_5xx",
-      - "v1/insights/lbaas/increase_in_http_error_rate_count_4xx",
-      - "v1/insights/lbaas/high_http_request_response_time",
-      - "v1/insights/lbaas/high_http_request_response_time_50p",
-      - "v1/insights/lbaas/high_http_request_response_time_95p",
-      - "v1/insights/lbaas/high_http_request_response_time_99p",
-      - "v1/dbaas/alerts/load_15_alerts",
-      - "v1/dbaas/alerts/memory_utilization_alerts",
-      - "v1/dbaas/alerts/disk_utilization_alerts",
-      - "v1/dbaas/alerts/cpu_alerts",
+      - "v1/insights/droplet/load_1"
+      - "v1/insights/droplet/load_5"
+      - "v1/insights/droplet/load_15"
+      - "v1/insights/droplet/memory_utilization_percent"
+      - "v1/insights/droplet/disk_utilization_percent"
+      - "v1/insights/droplet/cpu"
+      - "v1/insights/droplet/disk_read"
+      - "v1/insights/droplet/disk_write"
+      - "v1/insights/droplet/public_outbound_bandwidth"
+      - "v1/insights/droplet/public_inbound_bandwidth"
+      - "v1/insights/droplet/private_outbound_bandwidth"
+      - "v1/insights/droplet/private_inbound_bandwidth"
+      - "v1/insights/lbaas/avg_cpu_utilization_percent"
+      - "v1/insights/lbaas/connection_utilization_percent"
+      - "v1/insights/lbaas/droplet_health"
+      - "v1/insights/lbaas/tls_connections_per_second_utilization_percent"
+      - "v1/insights/lbaas/increase_in_http_error_rate_percentage_5xx"
+      - "v1/insights/lbaas/increase_in_http_error_rate_percentage_4xx"
+      - "v1/insights/lbaas/increase_in_http_error_rate_count_5xx"
+      - "v1/insights/lbaas/increase_in_http_error_rate_count_4xx"
+      - "v1/insights/lbaas/high_http_request_response_time"
+      - "v1/insights/lbaas/high_http_request_response_time_50p"
+      - "v1/insights/lbaas/high_http_request_response_time_95p"
+      - "v1/insights/lbaas/high_http_request_response_time_99p"
+      - "v1/dbaas/alerts/load_15_alerts"
+      - "v1/dbaas/alerts/memory_utilization_alerts"
+      - "v1/dbaas/alerts/disk_utilization_alerts"
+      - "v1/dbaas/alerts/cpu_alerts"
   value:
     description:
       - Value.
